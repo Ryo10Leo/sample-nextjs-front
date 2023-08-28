bcb107b42511b51f53621b07f52f2fdc3ab2a3ef
Ryo Iwata <ryo.bbl9@gmal.com>
2023-08-28 15:20:58 +0900

    aws-amplify導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import Link from "next/link";
 import AppBar from "@mui/material/AppBar";
@@ -19,11 +20,10 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import SupportIcon from "@mui/icons-material/Support";
 import LogoutIcon from "@mui/icons-material/Logout";
 import ThemeRegistry from "./components/ThemeRegistry/ThemeRegistry";
+import { AmplifyConf } from "./config/auth";
+import { Amplify } from "aws-amplify";
 
-export const metadata = {
-  title: "Next.js App Router + Material UI v5",
-  description: "Next.js App Router + Material UI v5",
-};
+Amplify.configure({ ...AmplifyConf });
 
 const DRAWER_WIDTH = 240;
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
@@ -8,8 +9,10 @@ import ListItem from "@mui/material/ListItem";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import MediaCard from "./components/MediaCard";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-export default function HomePage() {
+const HomePage = () => {
   return (
     <Box
       sx={{
@@ -74,4 +77,6 @@ export default function HomePage() {
       </Drawer>
     </Box>
   );
-}
+};
+
+export default withAuthenticator(HomePage);
